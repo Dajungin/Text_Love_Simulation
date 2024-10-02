79af29431fb90f9484aad29f500cc1fd33dfecac
Dajungin <jmj66638@gmail.com>
2024-10-02 21:27:31 +0900

    2024.10.02(최최종)
    콘솔창 크기 조절

--- Text-_Love_Simulation/Main screen/main.c
+++ Text-_Love_Simulation/Main screen/main.c
@@ -9,6 +9,10 @@
 //파일 크기
 
 
+
+
+
+
 void gotoxy(int x, int y); //커서 옮기기 미리 선언 
 
 enum {
@@ -75,7 +79,7 @@ void main()
 
 #pragma endregion 
 
-
+	system("mode con: cols=70 lines=30"); //cols==가로 , lines==세로
 
 
 	SetConsoleTitle(TEXT("Text_Love_Simulation_정민정 제작"));
@@ -130,10 +134,10 @@ void main()
 		Sleep(Time); //시간 지연으로 한다.
 		getchar();
 		clearConsole();
-		printf("\n[새로운 학교, 새로운 반.. 새로운 친구들.. 그리고 그렇게 정해진 나의 짝은.. 아까 부딪쳤던 그녀였다.]\n\n");
+		printf("\n[새로운 학교, 새로운 반.. 새로운 친구들..]\n[그리고 그렇게 정해진 나의 짝은.. 아까 부딪쳤던 그녀였다.]\n\n");
 		printf("안녕 잘 부탁해. 혹시 이름이 어떻게 돼?\n");
 
-		printf("??? : 너가 알아서 뭐하게?... 그리고 이건 물어보는 쪽이 먼저 이름을 말해야 하는거 아니야?\n");
+		printf("??? : 너가 알아서 뭐하게?...\n그리고 이건 물어보는 쪽이 먼저 이름을 말해야 하는거 아니야?\n");
 		printf("플레이 이름을 입력해주세요: ");
 		char name[20];
 		scanf("%s", &name);
@@ -157,16 +161,16 @@ void main()
 		switch (d)
 		{
 		case 1:
-			printf("아이리: 잘 맞출 수 있을지 누가 알아. 근데… 뭐, 너라면 잘 할 수 있을지도.\n");
+			printf("아이리: 잘 맞출 수 있을지 누가 알아.\n근데… 뭐, 너라면 잘 할 수 있을지도.\n");
 			love += 10;
 			break;
 		case 2:
-			printf("아이리: 흥, 빨리 끝내고 싶어하는 건 나도 마찬가지야. 너무 귀찮게 굴지 마.\n");
+			printf("아이리: 흥, 빨리 끝내고 싶어하는 건 나도 마찬가지야.\n너무 귀찮게 굴지 마.\n");
 			love += 0;
 			break;
 		case 3:
 
-			printf("아이리: 대충 하다니… 넌 정말 책임감이 없네. 그러고도 잘할 수 있다고 생각해?\n");
+			printf("아이리: 대충 하다니… 넌 정말 책임감이 없네.\n그러고도 잘할 수 있다고 생각해?\n");
 			love -= 10;
 			break;
 		default:
@@ -183,16 +187,16 @@ void main()
 		switch (e)
 		{
 		case 1:
-			printf("아이리: (살짝 놀란 표정) 혼자 먹는 게 더 편한데… 그래도, 네가 굳이 원한다면야..\n");
+			printf("아이리: (살짝 놀란 표정) 혼자 먹는 게 더 편한데…\n그래도, 네가 굳이 원한다면야..\n");
 			love += 10;
 			break;
 		case 2:
-			printf("아이리: 외롭긴 누가 외롭다는 거야! 혼자 있어도 괜찮거든?\n");
+			printf("아이리: 외롭긴 누가 외롭다는 거야!\n혼자 있어도 괜찮거든?\n");
 			love += 5;
 			break;
 		case 3:
 
-			printf("아이리: 그래, 너는 나한테 신경 안 써도 돼. 원래 혼자가 더 편해.\n");
+			printf("아이리: 그래, 너는 나한테 신경 안 써도 돼.\n원래 혼자가 더 편해.\n");
 			love -= 5;
 			break;
 		default:
@@ -219,7 +223,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: (작게 웃으며) 진짜로? 그럼… 조금 기대해볼게. 너무 부담은 주지 마.\n");
+				printf("아이리: (작게 웃으며) 진짜로? 그럼… 조금 기대해볼게.\n너무 부담은 주지 마.\n");
 		
 			}
 			else if (love >= 15)
@@ -230,7 +234,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: 뭐, 기대는 안 하지만… 네가 그렇게 말하니 한 번 믿어볼게.\n");
+				printf("아이리: 뭐, 기대는 안 하지만…\n네가 그렇게 말하니 한 번 믿어볼게.\n");
 			}
 				
 			else
@@ -241,7 +245,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: 흥, 굳이 네가 챙겨줄 거 없어. 나 혼자서도 충분하니까.\n");
+				printf("아이리: 흥, 굳이 네가 챙겨줄 거 없어.\n나 혼자서도 충분하니까.\n");
 			}
 				
 			love += 20;
@@ -255,7 +259,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: (작게 웃음) 그럼, 형식적이라도… 괜찮아. 너무 무리하지 않아도 돼.\n");
+				printf("아이리: (작게 웃음) 그럼, 형식적이라도… 괜찮아.\n너무 무리하지 않아도 돼.\n");
 			}
 				
 			else if (love >= 15)
@@ -266,7 +270,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: 그래, 너무 부담 주지 않는 게 좋겠지. 알아서 해.\n");
+				printf("아이리: 그래, 너무 부담 주지 않는 게 좋겠지.\n알아서 해.\n");
 			}
 			
 			else
@@ -276,7 +280,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 형식적? 차라리 아무것도 하지 않는 게 나을 것 같은데.\n");
+				printf("아이리: 형식적?\n차라리 아무것도 하지 않는 게 나을 것 같은데.\n");
 			}
 				
 			love += 0;
@@ -289,7 +293,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 필요 없진 않아… 그래도 네가 챙겨준다면 그건 좀 특별할지도 몰라.\n");
+				printf("아이리: 필요 없진 않아…\n그래도 네가 챙겨준다면 그건 좀 특별할지도 몰라.\n");
 			}
 				
 			else if (love >= 15)
@@ -299,7 +303,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 필요 없다니… 뭐, 그렇게 생각할 수도 있겠지만. 그래도, 네가 할 거면 하든가.\n");
+				printf("아이리: 필요 없다니… 뭐, 그렇게 생각할 수도 있겠지만.\n그래도, 네가 할 거면 하든가.\n");
 			}
 				
 			else
@@ -309,7 +313,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 당연하지. 난 이런 게임 따위에 신경 안 써. 그러니 아예 하지 마.\n");
+				printf("아이리: 당연하지. 난 이런 게임 따위에 신경 안 써.\n그러니 아예 하지 마.\n");
 			}
 				
 			love -= 10;
@@ -328,16 +332,16 @@ void main()
 		switch (f)
 		{
 		case 1:
-			printf("아이리: 친구들? 뭐, 어쩌다 보니… 그렇다고 특별히 친한 건 아니야.\n");
+			printf("아이리: 친구들? 뭐, 어쩌다 보니…\n그렇다고 특별히 친한 건 아니야.\n");
 			love += 5;
 			break;
 		case 2:
-			printf("아이리: (살짝 당황하며) 뭐? 나랑 어울린다고? 별로 신경 쓰지 마.\n");
+			printf("아이리: (살짝 당황하며) 뭐? 나랑 어울린다고?\n..별로 신경 쓰지 마.\n");
 			love += 10;
 			break;
 		case 3:
 
-			printf("아이리: 흥! 내가 너한테만 그런 건 아니야! 착각하지 마.\n");
+			printf("아이리: 흥! 내가 너한테만 그런 건 아니야!\n착각하지 마.\n");
 			love -= 5;
 			break;
 		default:
@@ -348,7 +352,7 @@ void main()
 		Sleep(Time);
 		getchar();
 		clearConsole();
-		printf("\n수학여행을 가게 되었다. 친구들과 즐겁게 놀다가 밤에 뒤쪽 산으로 담력시험을 하러 가기로 하였다\n뽑기로 짝을 뽑았는데 짝은 아이리였다.\n\n");
+		printf("\n수학여행을 가게 되었다.\n친구들과 즐겁게 놀다가 밤에 뒤쪽 산으로 담력시험을 하러 가기로 하였다\n뽑기로 정해진 짝은 아이리였다.\n\n");
 		printf("1.무서우면 내 손 잡아.같이 걷자.\n2.이런 거 무서워하는 거 아니지? 괜히 겁먹은 거 아니야?\n3.그래도 혹시 무서울 수도 있으니 내가 앞서 걸을게.\n");
 		scanf("%d", &c);
 		switch (c)
@@ -361,7 +365,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: (살짝 붉어진 얼굴로) 손? …뭐, 잡아도 괜찮아. 그치만, 너무 꽉 잡으면 안 돼.\n");
+				printf("아이리: (살짝 붉어진 얼굴로) 손? …뭐, 잡아도 괜찮아.\n그치만, 너무 꽉 잡으면 안 돼.\n");
 			}
 				
 			else if (love >= 45)
@@ -371,7 +375,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 손을 잡다니… 진짜 겁쟁이네. 그래도 뭐… 잡아도 괜찮아.\n");
+				printf("아이리: 손을 잡다니… 진짜 겁쟁이네.\n그래도 뭐… 잡아도 괜찮아.\n");
 			}
 				
 			else
@@ -382,7 +386,7 @@ void main()
 				printf("%s", buffer);
 				fclose(file);
 
-				printf("아이리: 손? 농담이지? 그런 거 필요 없어. 난 무섭지도 않고\n");
+				printf("아이리: 손? 농담이지? 그런 거 필요 없어.\n난 무섭지도 않고\n");
 			}
 				
 			love += 20;
@@ -395,7 +399,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: (작게) 겁… 조금 나긴 해. 하지만 너랑 같이 있으니까 괜찮아.\n");
+				printf("아이리: (작게) 겁… 조금 나긴 해.\n하지만 너랑 같이 있으니까 괜찮아.\n");
 			}
 				
 			else if (love >= 45)
@@ -405,7 +409,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 겁먹을 리 없잖아. 그치만… 네가 그렇게 말하니 이상하게 긴장되네.\n");
+				printf("아이리: 겁먹을 리 없잖아.\n그치만… 네가 그렇게 말하니 이상하게 긴장되네.\n");
 			}
 				
 			else
@@ -415,7 +419,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 무섭다고? 웃기지 마. 난 겁 같은 거 안 먹어.\n");
+				printf("아이리: 무섭다고? 웃기지 마.\n난 겁 같은 거 안 먹어.\n");
 			}
 				
 			love -= 10;
@@ -428,7 +432,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: (작게 웃으며) 앞서 가도 괜찮아. 그래도 가끔은 뒤도 돌아봐 줘. 혼자 있으면 좀… 무섭단 말이야.\n");
+				printf("아이리: (작게 웃으며) 앞서 가도 괜찮아. 그래도 가끔은 뒤도 돌아봐 줘.\n혼자 있으면 좀… 무섭단 말이야.\n");
 			}
 				
 			else if (love >= 45)
@@ -438,7 +442,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 앞서 걷는다니… 그래, 마음대로 해. 하지만 난 네가 필요 없으니까.\n");
+				printf("아이리: 앞서 걷는다니… 그래, 마음대로 해.\n하지만 난 네가 필요 없으니까.\n");
 			}
 				
 			else
@@ -448,7 +452,7 @@ void main()
 				fread(buffer, 1, Image, file);
 				printf("%s", buffer);
 				fclose(file);
-				printf("아이리: 네가 앞서든 뒤에 있든, 난 상관 없어. 그냥 빨리 끝내자.\n");
+				printf("아이리: 네가 앞서든 뒤에 있든, 난 상관 없어.\n그냥 빨리 끝내자.\n");
 			}
 				
 			love += 10;
@@ -475,27 +479,27 @@ void main()
 			setColor(YELLOW);
 			printf("해피 엔딩\n");
 			printf("[아이리와 나는 여러 사건을 겪으면서 가까워지고, 아이리는 자신의 성격을 극복하며 나에게 마음을 열어 주었다.]\n");
-			printf("아이리: 너랑 이렇게 지내다 보니… 알게 됐어.내가 네 곁에 있으면 더 이상 혼자가 아니란 걸.\n");
+			printf("아이리: 너랑 이렇게 지내다 보니… 알게 됐어.\n내가 네 곁에 있으면 더 이상 혼자가 아니란 걸.\n");
 			printf("%s : 그동안 고마웠어, 그리고 앞으로도 잘 부탁해\n", name);
-			printf("아이리: 정말… 나도 고마워. 너 덕분에 다시 사람을 믿을 수 있게 됐어.\n");
+			printf("아이리: 정말… 나도 고마워. \n너 덕분에 다시 사람을 믿을 수 있게 됐어.\n");
 			printf("게임을 플레이 해주셔서 감사합니다.");
 		}
 		else if (love >= 31)
 		{
 			setColor(GRAY);
 			printf("노멀 엔딩\n");
-			printf("[아이리와 조금 가까워지긴 했지만, 우리는 서로에게 완전히 마음을 열지는 못했다. 우리는 그렇게 서로 애매한 감정을 남긴 채 학교생활을 마무리하였다.]\n");
-			printf("아이리: 우리… 꽤 가까워지긴 했네. 하지만 여전히 뭔가 부족한 느낌이야. 그래도, 난 이제 혼자가 아니니까 괜찮아.\n");
+			printf("[아이리와 조금 가까워지긴 했지만, 우리는 서로에게 완전히 마음을 열지는 못했다. \n우리는 그렇게 서로 애매한 감정을 남긴 채 학교생활을 마무리하였다.]\n");
+			printf("아이리: 우리… 꽤 가까워지긴 했네. 하지만 여전히 뭔가 부족한 느낌이야.\n 그래도, 난 이제 혼자가 아니니까 괜찮아.\n");
 			printf("%s : 서로 더 노력했으면 좋았을 텐데.\n", name);
-			printf("아이리: (살짝 쓸쓸하게) 그래, 하지만 어쩌면 이게 우리에게 주어진 최선일지도 몰라.\n");
+			printf("아이리: (살짝 쓸쓸하게) 그래, 하지만 \n어쩌면 이게 우리에게 주어진 최선일지도 몰라.\n");
 			printf("게임을 플레이 해주셔서 감사합니다.");
 		}
 		else
 		{
 			setColor(RED);
 			printf("배드 엔딩\n");
-			printf("[아이리와 나는 서로 상처를 주며 신뢰를 쌓지 못하였다. 우리는 이렇게 관계의 끝이 왔다.]\n");
-			printf("아이리: 이제 와서 생각해보니… 우린 처음부터 맞지 않았던 것 같아. 너도 그렇지? 내가 너무 기대했나 봐.\n");
+			printf("[아이리와 나는 서로 상처를 주며 신뢰를 쌓지 못하였다. \n우리는 이렇게 관계의 끝이 왔다.]\n");
+			printf("아이리: 이제 와서 생각해보니… 우린 처음부터 맞지 않았던 것 같아. \n너도 그렇지? 내가 너무 기대했나 봐.\n");
 			printf("%s : 그래, 우리 여기까진 것 같아.\n", name);
 			printf("아이리: 알았어. 그럼, 이제 다신 마주칠 일 없겠지.\n");
 			printf("게임을 플레이 해주셔서 감사합니다.");
